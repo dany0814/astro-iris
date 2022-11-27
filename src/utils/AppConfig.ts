8bf600fb48ca62e298d75268c1f8e9aa90b1de3a
dany <jcanahuirev@gmail.com>
2022-11-27 00:07:33 -0300

    add navbar and footer component, AppConfig and install plugins for tailwind

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+    site_name: 'dany',
+    title: 'Dany profile',
+    description: 'astro and tailwind template for personal profile',
+    author: 'dany',
+    locale_region: 'en-us',
+    locale: 'en',
+  };
+  
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,23 @@ module.exports = {
 		colors: {
 			'primary': 'FF4646',
 		},
-		extend: {},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		  },
+		extend: {
+			spacing: {
+			  '8xl': '96rem',
+			  '9xl': '128rem',
+			},
+			borderRadius: {
+			  '4xl': '2rem',
+			}
+		  },
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/typography'),
+	  ],
 }
